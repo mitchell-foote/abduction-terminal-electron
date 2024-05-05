e82877db7739a80412c10f59a8a2af36b3fe6768
Mitchell Foote <mitchellfoote93@gmail.com>
2024-05-05 16:48:36 -0600

    added autoupdater and changed deploy script

--- src/main/index.ts
+++ src/main/index.ts
@@ -2,6 +2,7 @@ import { app, shell, BrowserWindow, ipcMain, globalShortcut } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
+import { autoUpdater } from 'electron-updater'
 
 function createWindow(): void {
     // Create the browser window.
@@ -37,6 +38,9 @@ function createWindow(): void {
     }
 }
 
+app.on('ready', function () {
+    autoUpdater.checkForUpdatesAndNotify()
+})
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
